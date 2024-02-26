83a6b06b9c1969ee45ade9c28161fbc2aa9f9ed6
ManoranjanJena24 <mjena@mitaoe.ac.in>
2024-02-26 12:55:32 +0530

    Intro to Middlewares
    Body Parser

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-// const http =require('http')
 const express = require('express')
 const app = express()
+const bodyParser=require('body-parser')
 
-
-app.use((req, res, next) => {
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use('/add-product', (req, res, next) => {
     console.log("Middleware 1");
-    next();//Allow the request to continue to next middleware in line
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button> </form>')
 
 })
-app.use((req, res, next) => {
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/')
+
+
+
+})
+
+
+app.use('/', (req, res, next) => {
     console.log("Middleware 2");
     res.send('<h1>Hello World</h1>')
 
 })
 
-// app.get('/', function (req, res) {
-//     res.send('Hello World')
-// // })
-// const server = http.createServer(app)
-app.listen(3000)
+
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000/")
+})
